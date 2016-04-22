f08416fc68c0500e1a850e09b5aadd8ec4129674
Guilherme Santos <guilherme@giox.com.br>
2016-04-22 11:55:30 -0300

    Change api url

--- app.js
+++ app.js
@@ -21,7 +21,7 @@
     app.use(express.static(path.join(__dirname, 'public')));
 
     app.use('/', routes);
-    app.use('/topics/v1', topics);
+    app.use('/api/v1/topics', topics);
 
     // catch 404 and forward to error handler
     app.use(function(req, res, next) {
