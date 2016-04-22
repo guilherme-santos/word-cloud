409ae4d8d8f26d2247fa4b32484ef9187a19fcd3
Guilherme Santos <guilherme@giox.com.br>
2016-04-22 12:24:10 -0300

    Indent

--- app.js
+++ app.js
@@ -1,58 +1,58 @@
 (function () {
-    "use strict";
-    
-    var express = require('express');
-    var path = require('path');
-    var logger = require('morgan');
-    var bodyParser = require('body-parser');
+  "use strict";
 
-    var routes = require('./routes/index');
-    var topics = require('./routes/topics');
+  var express = require('express');
+  var path = require('path');
+  var logger = require('morgan');
+  var bodyParser = require('body-parser');
 
-    var app = express();
+  var routes = require('./routes/index');
+  var topics = require('./routes/topics');
 
-    // view engine setup
-    app.set('views', path.join(__dirname, 'views'));
-    app.set('view engine', 'pug');
+  var app = express();
 
-    app.use(logger('dev'));
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(express.static(path.join(__dirname, 'public')));
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'pug');
 
-    app.use('/', routes);
-    app.use('/api/v1/topics', topics);
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(logger('dev'));
 
-    // catch 404 and forward to error handler
-    app.use(function(req, res, next) {
-      var err = new Error('Not Found');
-      err.status = 404;
-      next(err);
-    });
+  app.use('/', routes);
+  app.use('/api/v1/topics', topics);
 
-    // error handlers
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
 
-    // development error handler
-    // will print stacktrace
-    if (app.get('env') === 'development') {
-      app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-          message: err.message,
-          error: err
-        });
-      });
-    }
+  // error handlers
 
-    // production error handler
-    // no stacktraces leaked to user
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
       res.status(err.status || 500);
       res.render('error', {
         message: err.message,
-        error: {}
+        error: err
       });
     });
+  }
 
-    module.exports = app;
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {}
+    });
+  });
+
+  module.exports = app;
 })()
--- routes/topics.js
+++ routes/topics.js
@@ -1,30 +1,30 @@
 (function () {
-    "use strict";
+  "use strict";
 
-    var express = require('express');
-    var router = express.Router();
-    var topics = require('../topics.json');
-    
-    /* GET topics listing. */
-    router.get('/', function(req, res, next) {
-        res.json(topics.topics);
+  var express = require('express');
+  var router = express.Router();
+  var topics = require('../topics.json').topics;
+
+  /* GET topics listing. */
+  router.get('/', function(req, res, next) {
+    res.json(topics);
+  });
+
+  /* GET specfic topic. */
+  router.get('/:id', function(req, res, next) {
+    var topic = topics.find(function (topic) {
+      return topic.id == req.params.id
     });
-    
-    /* GET specfic topic. */
-    router.get('/:id', function(req, res, next) {
-        var topic = topics.topics.find(function (topic) {
-            return topic.id == req.params.id
-        });
-        
-        if (!topic) {
-            var err = new Error('Not Found');
-            err.status = 404;
-            next(err);
-            return
-        }
-        
-        res.json(topic);
-    });
+
+    if (!topic) {
+      var err = new Error('Not Found');
+      err.status = 404;
+      next(err);
+      return
+    }
+
+    res.json(topic);
+  });
 
     module.exports = router;
 })()
